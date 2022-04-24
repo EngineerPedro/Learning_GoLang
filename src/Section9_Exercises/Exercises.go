4420b28982e2b5911f152e59e92f727393e14d29
Pedro <plguzman1993@gmail.com>
2022-04-23 18:26:22 -0700

    Exercise 2 completed
    Was able to finish the exercise but had to look into some of the documentation that time for printing unicode

--- src/Section9_Exercises/Exercises.go
+++ src/Section9_Exercises/Exercises.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	Exercise_01()
+	//Exercise_01()
+	Exercise_02()
 }
 
 func Exercise_01() {
-	
+
 	/*Hands-on exercise #1
 		Print every number from 1 to 10,000
 	solution: https://play.golang.org/p/voDiuiDGGw
@@ -15,5 +16,29 @@ func Exercise_01() {
 
 	for x := 0; x <= 100; x++ {
 		fmt.Println(x)
+	}
+}
+
+func Exercise_02() {
+	/*Hands-on exercise #2
+		Print every rune code point of the uppercase alphabet three times. Your output should look like
+	this:
+		65
+		U+0041 'A'
+		U+0041 'A'
+		U+0041 'A'
+		66
+		U+0042 'B'
+		U+0042 'B'
+		U+0042 'B'
+		… through the rest of the alphabet characters
+	solution: https://play.golang.org/p/1OjnCX1D5H
+	video: 051*/
+
+	for i := 65; i <= 90; i++ {
+		fmt.Println(i)
+		for j := 0; j < 3; j++ {
+			fmt.Printf("\t%#U\n", i)
+		}
 	}
 }
