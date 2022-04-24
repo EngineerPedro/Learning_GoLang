a2120e90836c3b212be1b4208a18da04deae04c7
Pedro <plguzman1993@gmail.com>
2022-04-24 15:19:32 -0700

    Getting up to date
    Committing changes just to be able to pull
    Was working from a different computer

--- src/Section9_Exercises/Exercises.go
+++ src/Section9_Exercises/Exercises.go
@@ -7,7 +7,7 @@ func main() {
 }
 
 func Exercise_01() {
-	
+
 	/*Hands-on exercise #1
 		Print every number from 1 to 10,000
 	solution: https://play.golang.org/p/voDiuiDGGw
@@ -16,4 +16,8 @@ func Exercise_01() {
 	for x := 0; x <= 100; x++ {
 		fmt.Println(x)
 	}
+}
+
+func Exercise_02() {
+	
 }
