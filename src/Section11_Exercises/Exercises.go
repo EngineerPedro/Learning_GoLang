b56cb79f9e5f1664097108334d71a053763e50b4
Pedro <plguzman1993@gmail.com>
2022-04-29 11:55:06 -0700

    Completed 07 of Section 11
    Started Exercises for section 11 appending to the two dimensional slice ,and displaying every value at each of it's indexes

--- src/Section11_Exercises/Exercises.go
+++ src/Section11_Exercises/Exercises.go
@@ -10,7 +10,8 @@ func main() {
 	//Exercise_03()
 	//Exercise_04()
 	//Exercise_05()
-	Exercise_06()
+	//Exercise_06()
+	Exercise_07()
 }
 
 func Exercise_01() {
@@ -152,4 +153,31 @@ func Exercise_06() {
 	fmt.Println(x)
 	fmt.Println(len(x))
 	fmt.Println(cap(x))
+}
+
+func Exercise_07() {
+	/*Hands-on exercise #7
+		Create a slice of a slice of string ([][]string). Store the following data in the multi-dimensional
+	slice:
+		"James", "Bond", "Shaken, not stirred"
+		"Miss", "Moneypenny", "Helloooooo, James."
+		Range over the records, then range over the data in each record.
+			solution: https://play.golang.org/p/FMM4c2PhZg
+	video: 077*/
+
+	xs1 := []string{"James", "Bond", "Shaken, not stirred"}
+	xs2 := []string{"Miss", "Moneypenny", "Helloooooo, James."}
+	fmt.Println(xs1)
+	fmt.Println(xs2)
+
+	xxs := [][]string{xs1, xs2}
+	fmt.Println(xxs)
+
+	for i, xs := range xxs {
+		fmt.Println("record: ", i)
+		for j, val := range xs {
+			fmt.Printf("\t index position: %v \t value: \t %v \n", j, val)
+		}
+	}
+
 }
