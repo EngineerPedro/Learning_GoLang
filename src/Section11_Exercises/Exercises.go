12c8422326c24a951027a8964c1bbe1f468f44b1
Pedro <plguzman1993@gmail.com>
2022-04-29 09:56:26 -0700

    Completed 06 of Section 11
    Started Exercises for section 11 appending to the slice ,and displaying every value at each of it's indexes

--- src/Section11_Exercises/Exercises.go
+++ src/Section11_Exercises/Exercises.go
@@ -9,7 +9,8 @@ func main() {
 	//Exercise_02()
 	//Exercise_03()
 	//Exercise_04()
-	Exercise_05()
+	//Exercise_05()
+	Exercise_06()
 }
 
 func Exercise_01() {
@@ -125,4 +126,30 @@ func Exercise_05() {
 	x = append(x[:3], x[6:]...)
 	fmt.Println(x)
 
+}
+
+func Exercise_06() {
+	/*Hands-on exercise #6
+	Create a slice to store the names of all of the states in the United States of America. Use make
+	and append to do this. Goal: do not have the array that underlies the slice created more than
+	once. What is the length of your slice? What is the capacity? Print out all of the values, along
+	with their index position, without using the range clause. Here is a list of the 50 states:*/
+
+	x := make([]string, 50, 100)
+
+	y := []string{`Alabama`, `Alaska`, `Arizona`, `Arkansas`, `California`, `Colorado`, `Connecticut`,
+		`Delaware`, `Florida`, `Georgia`, `Hawaii`, `Idaho`, `Illinois`, `Indiana`, `Iowa`, `Kansas`, `Kentucky`,
+		`Louisiana`, `Maine`, `Maryland`, `Massachusetts`, `Michigan`, `Minnesota`, `Mississippi`, `Missouri`, `Montana`,
+		`Nebraska`, `Nevada`, `New Hampshire`, `New Jersey`, `New Mexico`, `New York`, `North Carolina`, `North Dakota`,
+		`Ohio`, `Oklahoma`, `Oregon`, `Pennsylvania`, `Rhode Island`, `South Carolina`, `South Dakota`, `Tennessee`, `Texas`,
+		`Utah`, `Vermont`, `Virginia`, `Washington`, `West Virginia`, `Wisconsin`, `Wyoming`}
+
+	for i := 0; i <= 49; i++ {
+		x[i] = y[i]
+		fmt.Println(i, x[i])
+	}
+
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
 }
