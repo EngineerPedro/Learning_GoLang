529f140677466fadc0b829832fa185c0ea89e521
Pedro <plguzman1993@gmail.com>
2022-04-29 08:24:02 -0700

    Completed 04 of Section 11
    Started Exercises for section 11 appending to the slice

--- src/Section11_Exercises/Exercises.go
+++ src/Section11_Exercises/Exercises.go
@@ -7,7 +7,8 @@ import (
 func main() {
 	//Exercise_01()
 	//Exercise_02()
-	Exercise_03()
+	//Exercise_03()
+	Exercise_04()
 }
 
 func Exercise_01() {
@@ -75,4 +76,34 @@ func Exercise_03() {
 	fmt.Println(x[2:7])
 	fmt.Println(x[1:6])
 
+}
+
+func Exercise_04() {
+	/*Hands-on exercise #4
+		Follow these steps:
+		● start with this slice
+		○ x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+		● append to that slice this value
+		○ 52
+		● print out the slice
+		● in ONE STATEMENT append to that slice these values
+		○ 53
+		○ 54
+		○ 55
+		● print out the slice
+		● append to the slice this slice
+		○ z := []int{56, 57, 58, 59, 60}
+		● print out the slice
+	solution: https://play.golang.org/p/QUYhtSBaDS
+	video: 074*/
+
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	x = append(x, 52)
+	fmt.Println(x)
+	y := []int{53, 54, 55}
+	x = append(x, y...)
+	fmt.Println(x)
+	z := []int{56, 57, 58, 59, 60}
+	x = append(x, z...)
+	fmt.Println(x)
 }
