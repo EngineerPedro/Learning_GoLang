3e088ccbc2be128874cd6dad08bf5aa6b66d4a9b
Pedro <plguzman1993@gmail.com>
2022-05-13 14:23:22 -0700

    Completed Exercise 2 Section 13
    Mixing it up with different types of data structures

--- src/Section13_Exercises/Exercises.go
+++ src/Section13_Exercises/Exercises.go
@@ -37,6 +37,44 @@ func exercise_02() {
 	/*	Hands-on exercise #2
 		Take the code from the previous exercise, then store the values of type person in a map with the key of last name.
 			Access each value in the map. Print out the values, ranging over the slice. solution: https://play.golang.org/p/RvvLyAMvGo*/
+	type person struct {
+		first      string
+		last       string
+		favFlavors []string
+	}
+	p1 := person{
+		first: "James",
+		last:  "Bond",
+		favFlavors: []string{
+			"chocolate",
+			"martini",
+			"rum and coke",
+		},
+	}
+
+	p2 := person{
+		first: "Miss",
+		last:  "Moneypenny",
+		favFlavors: []string{
+			"strawberry",
+			"vanilla",
+			"capuccino",
+		},
+	}
+
+	m := map[string]person{
+		p1.last: p1,
+		p2.last: p2,
+	}
+
+	for _, v := range m {
+		fmt.Println(v.first)
+		fmt.Println(v.last)
+		for i, val := range v.favFlavors {
+			fmt.Println(i, val)
+		}
+		fmt.Println("-------")
+	}
 }
 
 func exercise_03() {
