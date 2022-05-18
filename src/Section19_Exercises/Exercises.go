ee3c82a53637c438e804e2d7c6a6444b739e4ab7
Pedro <plguzman1993@gmail.com>
2022-05-18 09:59:31 -0700

    Completed 05 of Section 19
    Exercises making me mess with packages concerning sorting

--- src/Section19_Exercises/Exercises.go
+++ src/Section19_Exercises/Exercises.go
@@ -11,7 +11,8 @@ func main() {
 	//Exercise_01()
 	//Exercise_02()
 	//Exercise_03()
-	Exercise_04()
+	//Exercise_04()
+	Exercise_05()
 
 }
 
@@ -165,6 +166,74 @@ func Exercise_04() {
 	fmt.Println(xs)
 }
 
+func Exercise_05() {
+	/*
+		Hands-on exercise #5
+		Starting with this code, sort the []user by
+		● age
+		● last
+		Also sort each []string “Sayings” for each user
+		● print everything in a way that is pleasant
+		solution: https://play.golang.org/p/8RKkdtLl6w
+		video: 129
+	*/
+
+	u1 := user3{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
+
+	u2 := user3{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	u3 := user3{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+
+	users := []user3{u1, u2, u3}
+
+	fmt.Println(users)
+
+	// your code goes here
+
+	for _, u := range users {
+		fmt.Println(u.First, u.Last, u.Age)
+		for _, v := range u.Sayings {
+			fmt.Println("\t", v)
+		}
+	}
+
+	sort.Sort(ByAge(users))
+
+	for _, u := range users {
+		fmt.Println(u.First, u.Last, u.Age)
+		for _, v := range u.Sayings {
+			fmt.Println("\t", v)
+		}
+	}
+}
+
 //Work Area
 
 type user struct {
@@ -185,3 +254,22 @@ type user2 struct {
 	Age     int
 	Sayings []string
 }
+
+type user3 struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
+}
+
+type ByAge []user3
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
+type ByLast []user3
+
+func (l ByLast) Len() int           { return len(l) }
+func (l ByLast) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l ByLast) Less(i, j int) bool { return l[i].Last < l[j].Last }
